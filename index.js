e74d4f06582491851ff3bce9cfc5ef3f42eb2318
LAPTOP-MLVMM4OI\Vendryan <laboyng@gmail.com>
2022-06-05 22:36:33 +0700

    Exercise 3.5

--- index.js
+++ index.js
@@ -24,6 +24,9 @@ let persons = [
   }
 ]
 
+// DON'T FORGET TO CONVERT ID TO INTEGER
+// I FORGET EVERYTIME
+
 app.get('/info', (request, response) => {
   const time = new Date()
   response.end(`<div>Phone book has info for ${persons.length} people</div>` + 
@@ -44,6 +47,12 @@ app.get('/api/persons/:id', (request, response) => {
 
 app.get('/api/persons', (request, response) => {
   response.json(persons)  
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(p => p.id !== id)
+  response.status(204).end()
 })
 
 const PORT = 3001
