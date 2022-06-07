06e904b9638833047e6bc1de56b7bffb5ac60129
LAPTOP-MLVMM4OI\Vendryan <laboyng@gmail.com>
2022-06-07 18:44:20 +0700

    Exercise 3.15 and 3.16

--- index.js
+++ index.js
@@ -43,16 +43,22 @@ app.get('/info', (request, response) => {
                 `<div>${time}</div>`)
 })
 
-app.get('/api/persons/:id', (request, response) => {
-  Person.findById(request.params.id).then(person => {
-    response.json(person)
-  })
+app.get('/api/persons/:id', (request, response, next) => {
+  Person
+    .findById(request.params.id)
+    .then(person => {
+      response.json(person)
+    })
+    .catch(error => next(error))
 })
 
-app.get('/api/persons', (request, response) => {
-  Person.find({}).then(persons => {
-    response.json(persons)
-  }) 
+app.get('/api/persons', (request, response, next) => {
+  Person
+    .find({})
+    .then(persons => {
+      response.json(persons)
+    }) 
+    .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
@@ -71,11 +77,33 @@ app.post('/api/persons', (request, response) => {
   })
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(p => p.id !== id)
-  response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person
+    .findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+// Error handling need to be the last one in middleware using
+const errorHandler = (error, request, response, next) => {
+  console.log(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformed id' })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
